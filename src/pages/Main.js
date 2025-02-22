d4c93c3d8a91b304561edb58dfad38e7277d4e0c
Misho1234 <145994267+Bludii@users.noreply.github.com>
2025-02-22 15:02:49 +0400

    new

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,21 +3,80 @@ import styles from "./diffcss/Main.module.css";
 
 const Main = () => {
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.logo}>TEASER</div>
-      <ul className={styles.navLinks}>
-        <li className={styles.navcomponents}>HOME</li>
-        <li className={styles.navcomponents}>LISTING</li>
-        <li className={styles.navcomponents}>BLOCKS</li>
-        <li className={styles.navcomponents}>ABOUT</li>
-        <li className={styles.navcomponents}>PRICING</li>
-        <li className={styles.navcomponents}>CONTACT US</li>
-      </ul>
-      <div className={styles.authButtons}>
-        <button className={styles.signIn}>Sign In</button>
-        <button className={styles.signUp}>Sign Up</button>
+    <>
+      <div className="hero_area">
+        <header className="header_section">
+          <div className="container-fluid">
+            <nav className="navbar navbar-expand-lg custom_nav-container">
+              <a className="navbar-brand" href="index.html">
+                <img src="images/logo.png" alt="" />
+              </a>
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link" href="#home">
+                    HOME
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#listing">
+                    Listing
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#house">
+                    Blocks
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#about">
+                    ABOUT
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#price">
+                    PRICING
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#contact">
+                    CONTACT US
+                  </a>
+                </li>
+                <div className="User_option">
+                  <a href="login.html">Sign In</a>
+                  <a href="signup.html">Sign Up</a>
+                </div>
+              </ul>
+            </nav>
+          </div>
+        </header>
+        <section id="home" className="slider_section">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-md-4 offset-md-1">
+                <div className="detail-box">
+                  <h1>
+                    <span> Modern</span> <br />
+                    Apartment <br />
+                    House
+                  </h1>
+                  <p>
+                    It is a long established fact that a reader will be
+                    distracted by the readable content of
+                  </p>
+                  <div className="btn-box">
+                    <a href="" className="">
+                      {" "}
+                      Read More{" "}
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
-    </nav>
+    </>
   );
 };
 
